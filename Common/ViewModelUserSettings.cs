e85e588a00563a4792f6bca07609c69d0b99ef35
Рус <r.shayhilislamov55@gmail.com>
2024-09-25 21:41:50 +0500

    Модель представления данных пользователя.Шайхилисламов

---
+++ Common/ViewModelUserSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace Common
+{
+    public class ViewModelUserSettings
+    {
+        public string IPAddress { get; set; }
+        public string Port { get; set; }
+        public string Name { get; set; }
+        public int IdSnake = -1;
+
+    }
+}
