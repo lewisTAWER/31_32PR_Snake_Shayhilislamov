b1282d30db0320e30b2793eed09118a04abb6cf4
Рус <r.shayhilislamov55@gmail.com>
2024-09-25 21:20:33 +0500

    Созданный класс змеи.Шайхилисламов

---
+++ Common/Snakes.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace Common
+{
+    public class Snakes
+    {
+        public class Point
+        {
+            public int X { get; set; }
+            public int Y { get; set; }
+            public Point(int X, int Y)
+            {
+                this.X = X;
+                this.Y = Y;
+            }
+            public Point() { }
+        }
+        public enum Direction
+        {
+            Left,
+            Right,
+            Up,
+            Down,
+            Start
+        }
+        public List<Point> Points = new List<Point>();
+
+        public Direction direction = Direction.Start;
+
+        public bool GameOver = false;
+    }
+}
