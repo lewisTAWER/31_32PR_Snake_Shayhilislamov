826ba91a32d1a527f158013c3a566ef78d32ee69
Рус <r.shayhilislamov55@gmail.com>
2024-09-25 23:36:02 +0500

    Созданный метод загрузки данных о результатах игроков.Шайхилисламов

--- Snake_Shayhilislamov/Program.cs
+++ Snake_Shayhilislamov/Program.cs
@@ -261,5 +261,27 @@ namespace Snake_Shayhilislamov
             SW.WriteLine(json);
             SW.Close();
         }
+        public static void LoadLeaders()
+        {
+            if (File.Exists("./leaders.txt"))
+            {
+                StreamReader SR = new StreamReader("./leaders.txt");
+                string json = SR.ReadLine();
+                SR.Close();
+
+                if (!string.IsNullOrEmpty(json))
+                {
+                    Leaders = JsonConvert.DeserializeObject<List<Leaders>>(json);
+                }
+                else
+                {
+                    Leaders = new List<Leaders>();
+                }
+            }
+            else
+            {
+                Leaders = new List<Leaders>();
+            }
+        }
     }
 }
