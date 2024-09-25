929d2344ae6270db04bcbb3b30482395899f1abc
Рус <r.shayhilislamov55@gmail.com>
2024-09-25 20:48:35 +0500

    Добавленные переменные сервера.Шайхилисламов

--- Snake_Shayhilislamov/Program.cs
+++ Snake_Shayhilislamov/Program.cs
@@ -1,13 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace Snake_Shayhilislamov
 {
     internal class Program
     {
+        public static List<Leaders> Leaders = new List<Leaders>();
+        public static List<ViewModelUserSettings> remoteIPAddress = new List<ViewModelUserSettings>();
+        public static List<ViewModelGames> viewModelGames = new List<ViewModelGames>();
+        private static int localPort = 5001;
+        private static int MaxSpeed = 15;
+
         static void Main(string[] args)
         {
         }
