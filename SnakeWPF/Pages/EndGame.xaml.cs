700b228b36e827a8195178b69fcdb82a910cd9e4
Рус <r.shayhilislamov55@gmail.com>
2024-10-06 16:32:57 +0500

    Выполненная практическая работа на оценку хорошо.Шайхилисламов

--- SnakeWPF/Pages/EndGame.xaml.cs
+++ SnakeWPF/Pages/EndGame.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace SnakeWPF.Pages
 {
@@ -23,6 +11,16 @@ namespace SnakeWPF.Pages
         public EndGame()
         {
             InitializeComponent();
+            name.Content = MainWindow.mainWindow.ViewModelUserSettings.Name;
+            top.Content = MainWindow.mainWindow.ViewModelGames.Top;
+            glasses.Content = $"{MainWindow.mainWindow.ViewModelGames.SnakesPlayers.Points.Count - 3} glasses";
+            MainWindow.mainWindow.receivingUdpClient.Close();
+            MainWindow.mainWindow.tRec.Abort();
+            MainWindow.mainWindow.ViewModelGames = null;
+        }
+        private void OpenHome(object sender, RoutedEventArgs e)
+        {
+            MainWindow.mainWindow.OpenPage(MainWindow.mainWindow.Home);
         }
     }
 }
